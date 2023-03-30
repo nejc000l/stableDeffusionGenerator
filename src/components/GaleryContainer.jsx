9ab2d5f605796eb8af0f12e7fa61343ceeb272e4
nejc000l <56364838+nejc000l@users.noreply.github.com>
2023-03-30 11:43:18 +0200

    added media quarie

---
+++ src/components/GaleryContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/galeryContainer.css'
+function GaleryContainer() {
+  return (
+    <div className='galeryConatiner_main'>GaleryContainer</div>
+  )
+}
+
+export default GaleryContainer
